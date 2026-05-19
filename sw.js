0156b75ce3c82075c15aec848abfccb5f88458f9
unknown <drowssap56064904@gmail.com>
2026-05-19 15:38:42 +0800

    Site updated: 2026-05-19 15:38:42

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1779176285958";const preCache = ["/images/taichi.png","/images/koyuki-banner.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1779176318677";const preCache = ["/images/taichi.png","/images/koyuki-banner.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
