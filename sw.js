fa68d568a1448c9330eb4abed34f47fe0eaf3495
unknown <drowssap56064904@gmail.com>
2026-07-25 04:39:52 +0800

    Site updated: 2026-07-25 04:39:50

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1784924376894";const preCache = ["/images/taichi.png","/images/koyuki-banner.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1784925570767";const preCache = ["/images/taichi.png","/images/koyuki-banner.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
